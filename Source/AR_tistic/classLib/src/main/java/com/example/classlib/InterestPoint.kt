20398b7743640450aad7cfaed6c4b889781bc373
axdelafuen <axel.dlf@free.fr>
2022-10-07 20:35:03 +0200

    ajout class draw et interestPoint

---
+++ Source/AR_tistic/classLib/src/main/java/com/example/classlib/InterestPoint.kt
@@ -0,0 +1,8 @@
+package com.example.classlib
+
+data class InterestPoint(val id:Int,
+                         val name:String,
+                         val desc:String,
+                         val latitude:Double,
+                         val longitude:Double,
+                         val picture:String)
