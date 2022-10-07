5ad1ef185c713c77e98ffacf5dd86f5b65fa74c8
bajacqueli <bastien.jacquelin@etu.uca.fr>
2022-10-07 22:53:20 +0200

    Merge app and application

--- Source/AR_tistic/app/src/main/java/com/example/ar_tistic/MainActivity.kt
+++ Source/AR_tistic/app/src/main/java/com/example/ar_tistic/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.ar_tistic
 
+import com.example.classlib.*
+import com.example.stub.*
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +11,11 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import com.example.ar_tistic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,7 +25,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -32,6 +38,8 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+        checkLogPswd()
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -54,5 +62,37 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+    }
+    val stub = Stub()
+    private fun checkLogPswd(){
+        val connect = findViewById<Button>(R.id.LoginButton)
+        val log = findViewById<EditText>(R.id.LoginEdit)
+        val mdp = findViewById<EditText>(R.id.MdpEdit)
+        val err = findViewById<TextView>(R.id.ErrorLog)
+        connect.setOnClickListener {
+            err.visibility= View.GONE
+            val cttLog=log.text.toString()
+            val cttMdp=mdp.text.toString()
+            if (cttMdp.trim().isEmpty()||cttLog.trim().isEmpty()){
+                Toast.makeText(this,"l'Email ou mot de passe ne peut etre vide", Toast.LENGTH_LONG).show()
+            }
+            else{
+                if(!existLogPasswd(cttLog,cttMdp)){//log et mdp pas cohérents:
+                    err.visibility= View.VISIBLE
+                }
+                else{
+                    setContentView(R.layout.activity_main)
+                }
+            }
+        }
+    }
+    private fun existLogPasswd(name:String, pswd:String):Boolean{
+        val users= stub.loadData()
+        for (user in users){
+            if(user.name==name&&pswd==user.password||user.email==name&&pswd==user.password){
+                return true
+            }
+        }
+        return false
     }
 }
