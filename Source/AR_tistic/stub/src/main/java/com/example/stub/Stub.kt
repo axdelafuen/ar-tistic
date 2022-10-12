08e803f09cdd6e5b3470046ab962739eb4af5307
bajacqueli <bastien.jacquelin@etu.uca.fr>
2022-10-12 12:06:13 +0200

    Stub fonctionnel, idx pb

--- Source/AR_tistic/stub/src/main/java/com/example/stub/Stub.kt
+++ Source/AR_tistic/stub/src/main/java/com/example/stub/Stub.kt
@@ -7,8 +7,28 @@ import java.util.*
 import kotlin.collections.HashMap
 
 class Stub : IPersistancemanager{
-    init {
-        var UserHashMap=hashMapOf(
+
+    private var userHashMap=loadData().users
+        get() {
+            return field
+        }
+        set(value) {field=value}
+    private var intPtsHashMap=loadData().interestPoints
+        get() {return  field}
+        set(value) {field=value}
+    private var drawsHashMap=loadData().draws
+        get() {
+            return field
+        }
+      set(value) {field=value}
+
+    override fun loadData():(com.example.classlib.Collection){
+        var collec:com.example.classlib.Collection = Collection(loadUsers(),loadInterestPoints(),loadDraws())
+        return collec
+    }
+    fun loadUsers():HashMap<Int,User>{
+
+        return hashMapOf(
             0 to User(id= 0, name = "Alice", profilePicture = "./img/pp/Alice.jpg", email = "alice@alice.kt", password="1234", birthDate=Date(1999,2,2) , subscribes= hashMapOf(), nbReport = 0 ),
             1 to User(id= 1, name = "Fredo", profilePicture = "./img/pp/Fredo.jpg", email = "fred@fred.kt", password="aaaa123", birthDate= Date(2003,1,1), subscribes = hashMapOf(), nbReport = 0 ),
             2 to User(id= 2, name = "Patrick", profilePicture = "./img/pp/Fredo.jpg", email = "patrick@gmail.kt", password="aaaa123", birthDate= Date(2003,1,1), subscribes = hashMapOf(), nbReport = 0 ),
@@ -16,45 +36,20 @@ class Stub : IPersistancemanager{
             4 to User(id= 4, name = "Louis", profilePicture = "./img/pp/Fredo.jpg", email = "fred@fred.kt", password="aaaa123", birthDate= Date(2003,1,1), subscribes = hashMapOf(), nbReport = 0 ),
             5 to User(id= 5, name = "1", profilePicture = "./img/pp/Fredo.jpg", email = "2", password="1", birthDate= Date(2003,1,1), subscribes = hashMapOf(), nbReport = 0 ),
         )
-        var IntPtsHashMap= hashMapOf(
+    }
+    fun loadInterestPoints():HashMap<Int,InterestPoint>{
+        return  hashMapOf(
             0 to InterestPoint(id=0,name="IUT", desc="IUT de Clermont", latitude=1.0000, longitude=2.00000, picture="./img/intPoint/IUT.png"),
             1 to InterestPoint(id=1,name="MAISON", desc="IUT de Clermont", latitude=1.0000, longitude=2.00000, picture="./img/intPoint/IUT.png"),
             2 to InterestPoint(id=2,name="RUE", desc="IUT de Clermont", latitude=1.0000, longitude=2.00000, picture="./img/intPoint/IUT.png"),
         )
-        var DrawsHashMap = hashMapOf(
+    }
+    fun loadDraws():HashMap<Int,Draw>{
+        return hashMapOf(
             0 to Draw(id=0,name="Peinture bleu sur fond bleu", image = "./img/draw/0.jpg", interestPoint = hashMapOf(), creationDate = Date(1999,2,2), lifeTime = Time(24), authors = hashMapOf(), nbView = 60, nbReport = 0 ),
             1 to Draw(id=1,name="bleu", image = "./img/draw/0.jpg", interestPoint = hashMapOf(), creationDate = Date(1999,2,2), lifeTime = Time(24), authors = hashMapOf(), nbView = 60, nbReport = 0 ),
             2 to Draw(id=2,name="Peinture", image = "./img/draw/0.jpg", interestPoint = hashMapOf(), creationDate = Date(1999,2,2), lifeTime = Time(24), authors = hashMapOf(), nbView = 60, nbReport = 0 )
         )
-    }
-    var UserHashMap:HashMap<Int,User>
-        get() {
-            return UserHashMap
-        }
-        set(value) {}
-    var DrawsHashMap:HashMap<Int,Draw>
-        get() {
-            return DrawsHashMap
-        }
-        set(value) {}
-    var IntPtsHashMap:HashMap<Int,InterestPoint>
-        get() {
-            return IntPtsHashMap
-        }
-        set(value) {}
-
-    override fun loadData():(com.example.classlib.Collection){
-        var collec:com.example.classlib.Collection = Collection(loadUsers(),loadInterestPoints(),loadDraws())
-        return collec
-    }
-    fun loadUsers():HashMap<Int,User>{
-        return this.UserHashMap
-    }
-    fun loadInterestPoints():HashMap<Int,InterestPoint>{
-        return  this.IntPtsHashMap
-    }
-    fun loadDraws():HashMap<Int,Draw>{
-        return  this.DrawsHashMap
     }
     fun loadDataIdx(idxUser:Int, nbUser:Int,idxPt:Int, nbPt:Int,idxDraw: Int, nbDraw:Int):(com.example.classlib.Collection){
         var collec:com.example.classlib.Collection = Collection(loadUsersIndex(idxUser,nbUser),loadInterestPointsIndex(idxPt,nbPt),loadDrawsIndex(idxDraw,nbDraw))
@@ -62,16 +57,25 @@ class Stub : IPersistancemanager{
     }
     fun loadUsersIndex(idx:Int, nb:Int):HashMap<Int,User>{// Return of an hashmap with user in index position
         val ret = HashMap<Int, User>()// hashmap retourné avec la tranche de valeur demandé
-        for ((cpt, user) in UserHashMap.values.withIndex()){
-            if ((cpt >= idx) && (cpt<nb)){
-                ret[cpt] = user
+        var cpt=0
+        if(idx>userHashMap.size)return hashMapOf()//test si idx > nb user
+        for (user in userHashMap.values){
+
+
+
+            //fqire un while
+
+
+            if ((cpt >= idx) && (cpt==nb)){
+                ret.put(cpt,user)
+                cpt++
             }
         }
         return  ret
     }
     fun loadInterestPointsIndex(idx:Int, nb:Int):HashMap<Int,InterestPoint>{// Return of an hashmap with point of interest in index position
         val ret = HashMap<Int, InterestPoint>()// hashmap retourné avec la tranche de valeur demandé
-        for ((cpt, point) in IntPtsHashMap.values.withIndex()){
+        for ((cpt, point) in intPtsHashMap.values.withIndex()){
             if ((cpt >= idx) && (cpt<nb)){
                 ret[cpt] = point
             }
@@ -80,7 +84,7 @@ class Stub : IPersistancemanager{
     }
     fun loadDrawsIndex(idx:Int, nb:Int):HashMap<Int,Draw>{// Return of an hashmap with draws in index position
         val ret = HashMap<Int, Draw>()// hashmap retourné avec la tranche de valeur demandé
-        for ((cpt, draw) in DrawsHashMap.values.withIndex()){
+        for ((cpt, draw) in drawsHashMap.values.withIndex()){
             if ((cpt >= idx) && (cpt<nb)){
                 ret[cpt] = draw
             }
@@ -89,26 +93,26 @@ class Stub : IPersistancemanager{
     }
     //USERS FUNCTIONS
     fun getUserById(idUser:Int):User?{
-        return UserHashMap[idUser]
+        return userHashMap[idUser]
     }
     fun postUser(usr:User){
-        for ((cpt, user) in UserHashMap.values.withIndex()) {
+        for ((cpt, user) in userHashMap.values.withIndex()) {
             if (user == usr) {
-                UserHashMap[cpt] = user
+                userHashMap[cpt] = user
             }
         }
     }
     fun putUser(usr:User){
-        for ((cpt, user) in UserHashMap.values.withIndex()) {
+        for ((cpt, user) in userHashMap.values.withIndex()) {
             if (user == null) {
-                UserHashMap.put(cpt,usr)
+                userHashMap.put(cpt,usr)
             }
         }
     }
     fun deleteUser(usr:User){
-        for ((cpt, user) in UserHashMap.values.withIndex()) {
+        for ((cpt, user) in userHashMap.values.withIndex()) {
             if (user == usr) {
-                UserHashMap.remove(cpt)
+                userHashMap.remove(cpt)
             }
         }
     }
--- Source/AR_tistic/testConsol/src/main/java/com/example/testconsol/testConsol.kt
+++ Source/AR_tistic/testConsol/src/main/java/com/example/testconsol/testConsol.kt
@@ -4,18 +4,21 @@ import com.example.stub.*
 import java.util.*
 
 fun main(){
-    var UserHashMap=hashMapOf(
-        0 to User(id= 0, name = "Alice", profilePicture = "./img/pp/Alice.jpg", email = "alice@alice.kt", password="1234", birthDate= Date(1999,2,2) , subscribes= hashMapOf(), nbReport = 0 ),
-        1 to User(id= 1, name = "Fredo", profilePicture = "./img/pp/Fredo.jpg", email = "fred@fred.kt", password="aaaa123", birthDate= Date(2003,1,1), subscribes = hashMapOf(), nbReport = 0 ),
-        2 to User(id= 2, name = "Patrick", profilePicture = "./img/pp/Fredo.jpg", email = "patrick@gmail.kt", password="aaaa123", birthDate= Date(2003,1,1), subscribes = hashMapOf(), nbReport = 0 ),
-        3 to User(id= 3, name = "Nina", profilePicture = "./img/pp/Fredo.jpg", email = "fred@fred.kt", password="aaaa123", birthDate= Date(2003,1,1), subscribes = hashMapOf(), nbReport = 0 ),
-        4 to User(id= 4, name = "Louis", profilePicture = "./img/pp/Fredo.jpg", email = "fred@fred.kt", password="aaaa123", birthDate= Date(2003,1,1), subscribes = hashMapOf(), nbReport = 0 ),
-        5 to User(id= 5, name = "1", profilePicture = "./img/pp/Fredo.jpg", email = "2", password="1", birthDate= Date(2003,1,1), subscribes = hashMapOf(), nbReport = 0 ),
-    )
     var stub=Stub()
-    //Test User
-    //test get
-    for (usr in stub.loadUsers().values){
-        println(usr.name)
+    for(usr in stub.loadData().users){
+        println(usr.value.name)
+    }
+    println("----")
+    for(pt in stub.loadData().interestPoints){
+        println(pt.value.name)
+    }
+    println("----")
+    for(usr in stub.loadData().draws){
+        println(usr.value.name)
+    }
+    println("----")
+    var ret = stub.loadUsersIndex(1,1)
+    for(usrs in ret){
+        println(usrs.value.name)
     }
 }
