0cd09926d0d0124288474f96caaaf97f39c76a7b
bajacqueli <bastien.jacquelin@etu.uca.fr>
2022-10-23 14:45:56 +0200

    other merge pbs

--- Source/AR_tistic/classLib/src/main/java/com/example/classlib/Collection.kt
+++ Source/AR_tistic/classLib/src/main/java/com/example/classlib/Collection.kt
@@ -2,5 +2,7 @@ package com.example.classlib
 
 
 data class Collection (
-
+    var users:HashMap<Int,User>,
+    val interestPoints:HashMap<Int,InterestPoint>,
+    val draws:HashMap<Int,Draw>
     )
