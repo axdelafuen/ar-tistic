b4605cef52a085c9c16dbbf0598f07533440a37b
bajacqueli <bastien.jacquelin@etu.uca.fr>
2022-11-07 09:06:48 +0100

    Profil

--- Source/AR_tistic/app/src/main/java/com/example/ar_tistic/ProfilActivity.kt
+++ Source/AR_tistic/app/src/main/java/com/example/ar_tistic/ProfilActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
+import com.example.classlib.Date
+import com.example.classlib.User
 
 class ProfilActivity : AppCompatActivity() {
 
@@ -20,18 +22,36 @@ class ProfilActivity : AppCompatActivity() {
 
     }
     fun getInfos(){
-        var nomA=findViewById<TextView>(R.id.nameTxtView)
+        var email=findViewById<TextView>(R.id.emailTxtView)
         var pswdA=findViewById<TextView>(R.id.psswdTxtView)
+        var name=findViewById<TextView>(R.id.nameTxtView)
+        var birthDate=findViewById<TextView>(R.id.bdTxtView)
+
+
+
         val intent = intent
-        val email = intent.getSerializableExtra("email") as String?
-        val pswd = intent.getSerializableExtra("pswd") as String?
-        nomA.text=email
-        pswdA.text=pswd
+        // passer le manager
+        val cttemail = intent.getSerializableExtra("email") as String
+        val cttpswd = intent.getSerializableExtra("pswd") as String
+
+
+        var usr=createUser(cttemail,cttpswd)
+
+        email.text=cttemail
+        name.text=usr.name
+        birthDate.text=usr.birthDate.day.toString()+"-"+usr.birthDate.month.toString()+"-"+usr.birthDate.year.toString()
+
+        pswdA.text=cttpswd
     }
     fun returnMap(){
         val intent = Intent(applicationContext,MapActivity::class.java)
         startActivity(intent)
         finish()
+    }
+    fun createUser(email:String, pswd:String):User{
+        var id=1//Id from manager
+        val ppDefault="/img/ppDefault"
+        return User(id, "User"+id, "",email,pswd, Date(1999,2,2),hashMapOf(),0)
     }
 
 }
