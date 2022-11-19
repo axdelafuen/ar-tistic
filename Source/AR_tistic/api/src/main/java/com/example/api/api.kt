c654a59b97af7d227fc21c21aa3bd476f3bb6998
axdelafuen <axel.dlf@free.fr>
2022-11-19 20:46:08 +0100

    patch deploiement api

--- Source/AR_tistic/api/src/main/java/com/example/api/api.kt
+++ Source/AR_tistic/api/src/main/java/com/example/api/api.kt
@@ -13,7 +13,7 @@ fun main() {
 
             val app = Javalin.create().apply {
                 exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
-                error(404) { ctx -> ctx.json("not found") }
+                error(404) { ctx -> ctx.json("not found :(") }
             }.start(1705)
 
             app.routes {
