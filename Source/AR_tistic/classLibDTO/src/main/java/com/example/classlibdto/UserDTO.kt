b6b1e2da13df85919f6423fd10809f8e4de43eda
axdelafuen <axel.dlf@free.fr>
2022-11-20 12:36:59 +0100

    patch userDTO = bad constructor -> fixed

--- Source/AR_tistic/classLibDTO/src/main/java/com/example/classlibdto/UserDTO.kt
+++ Source/AR_tistic/classLibDTO/src/main/java/com/example/classlibdto/UserDTO.kt
@@ -6,9 +6,9 @@ import com.example.classlib.*
 data class UserDTO (
     val id: Int,
     val name: String,
-    val password :String,
     val profilePicture:String,
     val email: String,
+    val password :String,
     val birthDate: Date,
     val subscribes:HashMap<Int, User>,
     val nbReport:Int
