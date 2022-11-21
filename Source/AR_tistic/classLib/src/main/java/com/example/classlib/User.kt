762ddd8f281ad299545c7e70f1fcf8ca08ec14a0
Axel DE LA FUENTE <axel.de_la_fuente@etu.uca.fr>
2022-11-21 14:56:59 +0100

    Update 'Source/AR_tistic/classLib/src/main/java/com/example/classlib/User.kt'

--- Source/AR_tistic/classLib/src/main/java/com/example/classlib/User.kt
+++ Source/AR_tistic/classLib/src/main/java/com/example/classlib/User.kt
@@ -1,4 +1,5 @@
 package com.example.classlib
+
 import java.io.Serializable;
 
 class User(var id:Int, var name: String, var profilePicture:String="@drawable/pp_edit", var email: String, var password:String, var birthDate: Date, var subscribes:HashMap<Int,User>, var nbReport:Int): Serializable {
