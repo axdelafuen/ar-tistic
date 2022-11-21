c768665950cd3035ce83a3077c1dde9594867b84
Axel DE LA FUENTE <axel.de_la_fuente@etu.uca.fr>
2022-11-21 14:39:10 +0100

    revert aa59fd69917d14c512bc03d334c21ec9783465b6
    revert Update 'Source/AR_tistic/classLib/src/main/java/com/example/classlib/Draw.kt'

--- Source/AR_tistic/classLib/src/main/java/com/example/classlib/Draw.kt
+++ Source/AR_tistic/classLib/src/main/java/com/example/classlib/Draw.kt
@@ -1,28 +1,16 @@
 package com.example.classlib
 
+import User
 import java.sql.Time
 import kotlin.collections.HashMap
 
-class Draw{
-    val id:Int
-    val name:String
-    val image:String
-    val interestPoint: HashMap<Int,InterestPoint>
-    val creationDate: Date
-    val lifeTime: Time
-    val authors:HashMap<Int,User>
-    val nbView:Int
-    val nbReport:Int
-
-    constructor(id:Int, name:String, image:String, interestPoint: HashMap<Int,InterestPoint>, creationDate:Date, lifeTime: Time, authors:HashMap<Int,User>, nbView:Int, nbReport:Int){
-        this.id = id
-        this.name = name
-        this.image = image
-        this.interestPoint = interestPoint
-        this.creationDate = creationDate
-        this.lifeTime = lifeTime
-        this.authors = authors
-        this.nbView = nbView
-        this.nbReport = nbReport
-    }
-}
+data class Draw(
+    val id:Int,
+    val name:String,
+    val image:String,
+    val interestPoint: HashMap<Int,InterestPoint>,
+    val creationDate: Date,
+    val lifeTime: Time,
+    val authors:HashMap<Int,User>,
+    val nbView:Int,
+    val nbReport:Int):java.io.Serializable
