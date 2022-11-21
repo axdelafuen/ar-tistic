d69e35bfb3301581195384ae71e1b307d9203c5a
Axel DE LA FUENTE <axel.de_la_fuente@etu.uca.fr>
2022-11-21 14:42:12 +0100

    Update 'Source/AR_tistic/classLib/src/main/java/com/example/classlib/IPersistenceManager.kt'

--- Source/AR_tistic/classLib/src/main/java/com/example/classlib/IPersistenceManager.kt
+++ Source/AR_tistic/classLib/src/main/java/com/example/classlib/IPersistenceManager.kt
@@ -1,12 +1,21 @@
 package com.example.classlib
 
-interface IPersistenceManager : java.io.Serializable{
+import User
+import java.io.Serializable
+
+interface IPersistenceManager : Serializable {
     var userHashMap:HashMap<Int,User>
     var intPtsHashMap:HashMap<Int,InterestPoint>
     var drawsHashMap:HashMap<Int,Draw>
+    // USER METHODS
     fun loadData():(com.example.classlib.Collection)
     fun getUserById(idUser:Int):User?
     fun createUser(usr:User)
     fun updateUser(id:Int,usr:User)
     fun deleteUser(id:Int)
+    fun findUserByLogPswd(log:String, psswrd:String):User
+
+    // DRAWS METHODS
+
+    // INTERESTPOINTS METHODS
 }
