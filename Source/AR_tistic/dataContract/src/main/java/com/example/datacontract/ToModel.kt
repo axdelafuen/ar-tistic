a6d4c09468f4b9839e56c7be7ce4836fff08936c
axdelafuen <axel.dlf@free.fr>
2022-11-21 15:50:06 +0100

    patch dataContract

--- Source/AR_tistic/dataContract/src/main/java/com/example/datacontract/ToModel.kt
+++ Source/AR_tistic/dataContract/src/main/java/com/example/datacontract/ToModel.kt
@@ -4,5 +4,5 @@ import com.example.classlib.*
 import com.example.classlibdto.*
 
 fun toModel(user:UserDTO):User{
-   return User(user.id,user.name,user.profilePicture, user.email,user.password,user.birthDate,user.subscribes,user.nbReport);
+   return User(user.id,user.email,user.profilePicture, user.name,user.password,user.birthDate,user.subscribes,user.nbReport);
 }
