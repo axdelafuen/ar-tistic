3e4212f52f6859aaa89c3516f4926525aae0d0d4
Axel DE LA FUENTE <axel.de_la_fuente@etu.uca.fr>
2022-11-21 14:46:58 +0100

    Update 'Source/AR_tistic/classLib/src/main/java/com/example/classlib/Conversation.kt'

--- Source/AR_tistic/classLib/src/main/java/com/example/classlib/Conversation.kt
+++ Source/AR_tistic/classLib/src/main/java/com/example/classlib/Conversation.kt
@@ -1,6 +1,5 @@
 package com.example.classlib
 
-
 data class Conversation(val id:Int,
                         val participant:HashMap<Int,User>,
                         val messages:HashMap<Int,Message>
