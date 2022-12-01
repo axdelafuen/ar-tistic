1b331290de97fbf6f3d21a226adcead611425ee9
Bastien Jacquelin <bastien.jacquelin@etu.uca.fr>
2022-12-01 14:33:26 +0100

    last updates UI

--- Source/AR_tistic/app/src/main/java/com/example/ar_tistic/EditProfileActivity.kt
+++ Source/AR_tistic/app/src/main/java/com/example/ar_tistic/EditProfileActivity.kt
@@ -1,41 +1,55 @@
 package com.example.ar_tistic
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.TextView
-import com.example.classlib.Date
-import com.example.classlib.Manager
-import com.example.classlib.User
+import android.widget.*
+import com.example.classlib.*
 
 class EditProfileActivity : AppCompatActivity() {
-    lateinit var manager: Manager
+    lateinit var manager:Manager
+    lateinit var button: Button
     lateinit var imageView: ImageView
 
+    companion object{
+        val IMAGE_REQUEST_CODE = 100
+    }
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
-        manager = intent.getSerializableExtra("manager") as Manager
         getInfos()
         var retMap=findViewById<ImageButton>(R.id.returnMapPage)
         retMap.setOnClickListener(){
             returnMap()
         }
+        var upload=findViewById<Button>(R.id.upload)
+        button.setOnClickListener{
+            pickImageGallery()
+            /*Press alt enter*/
+        }
+        upload.setOnClickListener(){
+            val intent = Intent()
+                .setType("*/*")
+                .setAction(Intent.ACTION_GET_CONTENT)
+            startActivityForResult(Intent.createChooser(intent, "Select a file"), 777)
+        }
     }
-
     fun getInfos(){
-        var email=findViewById<TextView>(R.id.email)
+        var email=findViewById<TextView>(R.id.emailTxtView)
         var name=findViewById<TextView>(R.id.nameTxtView)
-        var date=findViewById<TextView>(R.id.bdTxtView)
+        var birthDate=findViewById<TextView>(R.id.bdTxtView)
         val intent = intent
         //Get manager
         manager = intent.getSerializableExtra("manager") as Manager
-        var usr: User =manager.usr
-        email.text=manager.usr.email
-        name.text=manager.usr.name
-        date.text=manager.usr.birthDate.day.toString()+"-"+manager.usr.birthDate.month.toString()+"-"+manager.usr.birthDate.year.toString()
+        var usr:User=manager.usr
+        email.text=usr.email
+        name.text=usr.name
+        birthDate.text=usr.birthDate.day.toString()+"-"+usr.birthDate.month.toString()+"-"+usr.birthDate.year.toString()
+        button=findViewById(R.id.upload)
         imageView=findViewById(R.id.imageView2)
     }
     fun returnMap(){
@@ -44,12 +58,12 @@ class EditProfileActivity : AppCompatActivity() {
     fun pickImageGallery(){
         val intent = Intent(Intent.ACTION_PICK)
         intent.type="image/*"
-        startActivityForResult(intent, ProfilActivity.IMAGE_REQUEST_CODE)
+        startActivityForResult(intent, IMAGE_REQUEST_CODE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode== ProfilActivity.IMAGE_REQUEST_CODE && resultCode == RESULT_OK){
+        if(requestCode== IMAGE_REQUEST_CODE && resultCode == RESULT_OK){
             imageView.setImageURI(data?.data)
         }
     }
