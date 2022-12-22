f8e25716ddef784b11c517238f696c7b5add3297
axdelafuen <axel.dlf@free.fr>
2022-12-22 22:43:51 +0100

    patch data contract

--- Source/AR_tistic/api/src/main/java/com/example/api/api.kt
+++ Source/AR_tistic/api/src/main/java/com/example/api/api.kt
@@ -38,7 +38,7 @@ fun main() {
                     ctx.json("forbidden").status(403)
                 }
                 else{
-                    ctx.json(res)
+                    ctx.json(toDTO(res))
                 }
             }
             get("/users/{user-id}") { ctx ->
@@ -61,7 +61,7 @@ fun main() {
             }
             get("/users/findByPattern/{pattern}"){ctx->
                 val res = data.patternRecognitionUsers(ctx.pathParam("pattern").toString())
-                //ctx.json(toDTO(res!!))
+                ctx.json(res!!)
             }
 
             /*
