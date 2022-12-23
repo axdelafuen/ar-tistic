126ff174a6a5c00ce0dac7bbbcfe5a6268ee4e10
bajacqueli <bastien.jacquelin@etu.uca.fr>
2022-12-23 19:08:27 +0100

    try : pick img on gallery and put it

--- Source/AR_tistic/app/src/main/java/com/example/ar_tistic/EditProfileActivity.kt
+++ Source/AR_tistic/app/src/main/java/com/example/ar_tistic/EditProfileActivity.kt
@@ -1,6 +1,7 @@
 package com.example.ar_tistic
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -18,15 +19,9 @@ import java.util.*
 
 class EditProfileActivity : AppCompatActivity() {
     lateinit var manager:Manager
-    lateinit var button: Button
     lateinit var pp: ImageView
 
-    companion object{
-        val IMAGE_REQUEST_CODE = 100
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    @SuppressLint("MissingInflatedId")
+    private val GALLERY_REQUEST_CODE = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
@@ -36,15 +31,8 @@ class EditProfileActivity : AppCompatActivity() {
             returnMap()
         }
         var upload=findViewById<Button>(R.id.upload)
-        button.setOnClickListener{
-            pickImageGallery()
-            /*Press alt enter*/
-        }
-        upload.setOnClickListener(){
-            val intent = Intent()
-                .setType("*/*")
-                .setAction(Intent.ACTION_GET_CONTENT)
-            startActivityForResult(Intent.createChooser(intent, "Select a file"), 777)
+        upload.setOnClickListener{
+            openGallery()
         }
     }
     @RequiresApi(Build.VERSION_CODES.O)
@@ -59,8 +47,6 @@ class EditProfileActivity : AppCompatActivity() {
         email.text=usr.email
         name.text=usr.name
         birthDate.text=usr.birthDate.day.toString()+"-"+usr.birthDate.month.toString()+"-"+usr.birthDate.year.toString()
-        button=findViewById(R.id.upload)
-
     }
     fun returnMap(){
         finish()
@@ -79,16 +65,18 @@ class EditProfileActivity : AppCompatActivity() {
         val decodedImage = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
         pp.setImageBitmap(decodedImage)
     }
-    fun pickImageGallery(){
+        private fun openGallery() {
         val intent = Intent(Intent.ACTION_PICK)
-        intent.type="image/*"
-        startActivityForResult(intent, IMAGE_REQUEST_CODE)
+        intent.type = "image/*"
+        startActivityForResult(intent, GALLERY_REQUEST_CODE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode== IMAGE_REQUEST_CODE && resultCode == RESULT_OK){
-            pp.setImageURI(data?.data)
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            val selectedImageUri = data.data
+            val imageView = findViewById<ImageView>(R.id.imageView2)
+            imageView.setImageURI(selectedImageUri)
         }
     }
 }
